acf651a52d60b51678e50a71da7f2fd54d23b8f1
msuthar5 <msuthar@hawk.iit.edu>
2019-11-22 10:02:04 -0600

    gesture detecter

--- gesture_detector.py
+++ gesture_detector.py
@@ -2,13 +2,25 @@ import numpy as np
 class GestureDetector:
     def __init__(self):
         self.value = 1
+        self.records=list()
+        self.skip=0
     
-    def detect(self, state):
+    def detect(self, state,window_size=8,epsilon=7):
 
+        if len(state)<(window_size-1):
+        	return False
+        if self.skip > 0:
+        	self.skip-=1
+        	return False
+
+        state=sorted(state, key = lambda i: i['accel_time'])
         zs = [d['accel_z'] for d in state]
-        if len(zs) < 1:
-            return False
-        if abs(zs[0]) < 0.15:
-            return True
-        
-        
+        xs = [d['accel_x'] for d in state]
+        times = [d['accel_time'] for d in state]
+        coef=np.polyfit(times,xs,2)
+        if coef[0] > epsilon:
+        	self.skip=window_size-1
+        	print("DATA:" + str(xs))
+        	return True
+        else:
+        	return False
